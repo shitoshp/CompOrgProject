97df2a98ee2651bac02a3c8d673174ba1e569ac7
shitoshp <shitosh@yahoo.com>
2015-09-22 22:14:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -16,49 +16,66 @@ var a = angular.module('app', ['ionic']);  //creating app by angular
 
 
         
-a.service('ParseHttpService', function ($q) {
+a.service('ParseHttpService', function ($http) {
         
-		var stuffList = [];
-        stuffList.push({
-            "hero": "Storm Spirit",
-            "type": "Int"
-        });
-        stuffList.push({
-            "hero": "Faceless Void",
-            "type": "Agility"
-        });
-        stuffList.push({
-            "hero": "Puck",
-            "type": "Int"
-        });
-        stuffList.push({
-            "hero": "Axe",
-            "type": "Strength"
-        });
-        
-
+		var baseURL = "https://api.parse.com/1/";
+        var authenticationHeaders = {
+        "x-parse-application-id": "jYWwyHcSTkapjexeYxmYFJd5MpOGKReBvE3zZJUG",
+        "x-parse-rest-api-key": "DkB9Sm1KWoatEdcCxmmV3zCEa4xrdVogXuFcH25i"
+    	};
 
         return {
-       	/**
-            * returns all of the data
-            */
-            getAllItems: function () {
-                var deferred = $q.defer();
-                setTimeout(function () {
-                    deferred.resolve(stuffList);
-                }, 500);
-                return deferred.promise;
-            }
             /**
-            * Gets item by specific id
-            * @param   {Number}  _itemId index in the array
-            * @returns {Promise}
-            */
-            
+             * [[Description]]
+             * @returns {Promise} [[Description]]
+             */
+            login: function () {
 
+                var credentials = {
+                    "username": "admin",
+                    "password": "test"
+                };
+
+                var settings = {
+                    method: 'GET',
+                    url: baseURL + 'login',
+                    headers: authenticationHeaders,
+                    // params are for query string parameters
+                    params: credentials
+                };
+
+                // $http returns a promise, which has a then function,
+                // which also returns a promise
+                return $http(settings)
+                    .then(function (response) {
+                        // In the response resp.data contains the result
+                        // check the console to see all of the data returned
+                        console.log('login', response);
+                        return response.data;
+                    });
+            },
+
+            getStuff: function (_id) {
+
+                // if an id is passed in then use it when querying
+                // stuff data
+                var settings = {
+                    method: 'GET',
+                    url: baseURL + 'classes/stuff/' + _id,
+                    headers: authenticationHeaders,
+                };
+
+                // $http returns a promise, which has a then function,
+                // which also returns a promise
+                return $http(settings)
+                    .then(function (response) {
+                        // In the response resp.data contains the result
+                        // check the console to see all of the data returned
+                        console.log('getStuff', response);
+                        return response.data;
+                    });
+            }
         };
-        
-
 }); 
 
 
@@ -66,11 +83,34 @@ a.service('ParseHttpService', function ($q) {
 
 a.controller('AppCtrl', function ($scope, ParseHttpService) {
 
-	$scope.stuffList = [];
+	 $scope.currentUser = null;
+        $scope.apiResponseData = {};
+        $scope.uiData = {};
 
-	ParseHttpService.getAllItems().then(function (_data) {
-        $scope.stuffList = _data;
-    });
+        function _alertHandler(_error) {
+            alert("ERROR " + JSON.stringify(_error, null, 2));
+        }
+        /**
+         * logs the user into the application
+         */
+        $scope.doLogin = function () {
+            return ParseHttpService.login().then(function (_response) {
+                $scope.currentUser = _response;
+                $scope.apiResponseData = _response;
+            }, _alertHandler);
+        };
+
+        /**
+         * [[Description]]
+         * @returns {[[Type]]} [[Description]]
+         */
+
+        $scope.getStuffList = function () {
+            return ParseHttpService.getStuff("").then(function (_response) {
+                $scope.apiResponseData = _response;
+            }, _alertHandler);
+        }; 
+
 });	
 
 
