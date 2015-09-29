7fc0560cee48e7a06d7a7162a2ad7bd3c1f55b38
shitoshp <shitosh@yahoo.com>
2015-09-29 01:09:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-
-var a = angular.module('app', ['ionic']);  //creating app by angular
+(function () {
+    
+    var a = angular.module('app', ['ionic', 'App.Services']);  //creating app by angular
 /*.controller("controller-one", function($scope){
     
 	//Private since not attached to scope
@@ -16,86 +17,29 @@ var a = angular.module('app', ['ionic']);  //creating app by angular
 
 
         
-a.service('ParseHttpService', function ($http) {
-        
-		var baseURL = "https://api.parse.com/1/";
-        var authenticationHeaders = {
-        "x-parse-application-id": "jYWwyHcSTkapjexeYxmYFJd5MpOGKReBvE3zZJUG",
-        "x-parse-rest-api-key": "DkB9Sm1KWoatEdcCxmmV3zCEa4xrdVogXuFcH25i"
-    	};
 
-        return {
-            /**
-             * [[Description]]
-             * @returns {Promise} [[Description]]
-             */
-            login: function () {
 
-                var credentials = {
-                    "username": "admin",
-                    "password": "test"
-                };
 
-                var settings = {
-                    method: 'GET',
-                    url: baseURL + 'login',
-                    headers: authenticationHeaders,
-                    // params are for query string parameters
-                    params: credentials
-                };
 
-                // $http returns a promise, which has a then function,
-                // which also returns a promise
-                return $http(settings)
-                    .then(function (response) {
-                        // In the response resp.data contains the result
-                        // check the console to see all of the data returned
-                        console.log('login', response);
-                        return response.data;
-                    });
-            },
+        a.controller('AppCtrl', function ($scope, ParseHttpService) {
 
-            getStuff: function () {
+	       $scope.stuffList = {};
 
-                // if an id is passed in then use it when querying
-                // stuff data
-                var settings = {
-                    method: 'GET',
-                    url: baseURL + 'classes/stuff/',
-                    headers: authenticationHeaders,
-                };
+            ParseHttpService.login().then(function(){
 
-                // $http returns a promise, which has a then function,
-                // which also returns a promise
-                return $http(settings)
-                    .then(function (response) {
-                        // In the response resp.data contains the result
-                        // check the console to see all of the data returned
-                        console.log('getStuff', response);
-                        return response.data.results;
-                    });
+            });
+
+            $scope.handleButtonClick = function() {
+                ParseHttpService.getStuff().then(function (_data) {
+                    $scope.stuffList = _data;
+                }); 
             }
-        }
-}); 
-
-
-
-
-a.controller('AppCtrl', function ($scope, ParseHttpService) {
-
-	$scope.stuffList = {};
-
-    ParseHttpService.login().then(function(){
-
-    });
-
-    $scope.handleButtonClick = function() {
-        ParseHttpService.getStuff().then(function (_data) {
-            $scope.stuffList = _data;
-        }); 
-    }
 
 	
 
-});	
+        });	
+
+})();
+
+
 
