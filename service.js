5dcf66dd1b95fcc0a11886828b86e22cea79b4a1
Shitosh Parajuli <shitosh@yahoo.com>
2015-10-02 16:46:29 -0400

    Update service.js

--- service.js
+++ service.js
@@ -63,7 +63,7 @@
                         // In the response resp.data contains the result
                         // check the console to see all of the data returned
                         console.log('getStuff', response);
-                        return response.data.results;
+                        return response.data;
                     });
             },
 
@@ -73,7 +73,7 @@
                 var settings = {
                     method: 'GET',
                     url: baseURL + 'classes/stuff/',
-                    headers: authenticationHeaders,
+                    headers: authenticationHeaders
                 };
 
 
@@ -81,8 +81,78 @@
                     .then(function (response) {
                         // In the response resp.data contains the result
                         // check the console to see all of the data returned
-                        console.log('getStuff', response);
+                        console.log('getStuffbyid', response);
                         return response.data.results;
+                    });
+            },
+
+            deleteObjectById: function (_id) {
+
+                var settings = {
+                    headers: authenticationHeaders
+                };
+
+                // $http returns a promise, which has a then function,
+                // which also returns a promise
+                return $http.delete(baseURL + 'classes/stuff/' + _id, settings)
+                    .then(function (response) {
+                        // In the response resp.data contains the result
+                        // check the console to see all of the data returned
+                        console.log('deleteObjectById', response);
+                        return response.data;
+                    });
+            },
+
+            addObject: function (_params){
+
+                var settings = {
+                    headers: authenticationHeaders
+                };
+
+                var dataObject;
+                dataObject = {
+                    "name": _params.name,
+                    "room": _params.room
+                };
+
+                var dataObjectString = JSON.stringify(dataObject);
+
+
+
+                // $http returns a promise, which has a then function,
+                // which also returns a promise
+                return $http.post(baseURL + 'classes/stuff', dataObjectString, settings)
+                    .then(function (response) {
+                        // In the response resp.data contains the result
+                        // check the console to see all of the data returned
+                        console.log('addObject', response);
+                        return response.data;
+                    });
+            },
+
+            updateObject: function (_params) {
+
+
+                var settings = {
+                    headers: authenticationHeaders
+                };
+
+                var dataObject = {
+                    "name": (_params.name ? _params.name : JSON.null),
+                    "room": (_params.room ? _params.room : JSON.null)
+                };
+
+                var dataObjectString = JSON.stringify(dataObject);
+
+                var apiUrl = baseURL + 'classes/stuff/' + _params.objectId;
+
+
+                return $http.put(apiUrl, dataObjectString, settings)
+                    .then(function (response) {
+                        // In the response resp.data contains the result
+                        // check the console to see all of the data returned
+                        console.log('updateObject', response);
+                        return response.data;
                     });
             }
         }
