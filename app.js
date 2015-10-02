b3ec3c84ddaceece57cca89ca13f92b84487c87d
Shitosh Parajuli <shitosh@yahoo.com>
2015-10-02 16:47:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -21,15 +21,134 @@
 
 
 
+    a.config(function($stateProvider, $urlRouterProvider){
+        //Setting default to home
+        $urlRouterProvider.otherwise("/home");
+
+        //Setting up states
+        $stateProvider
+            .state('home', {
+                url: "/home",
+                templateUrl: "home.html",
+                controller: "homeCtrl"
+            })
+            .state('detail', {
+                url: "/detail/:id",
+                templateUrl: "detail.html",
+                controller: "detailCtrl"
+            });
+    });
 
 
 
-    a.controller('AppCtrl', function ($scope, ParseHttpService) {
+    a.controller('homeCtrl', function ($scope, $state, ParseHttpService) {
+        $scope.stateInfo = $state.current;
 
-        $scope.stuffList = {};
+        $scope.itemsList = {};
+
+        $scope.inputItem = {
+            value: "",
+            name: "",
+            room: ""
+        };
+
+        $scope.goToDetailState = function(_id) {
+            $state.go("detail", {
+                id: _id
+            });
+        };
+
+        $scope.deleteObject = function editObject(_objectId) {
+            ParseHttpService.deleteObjectById(_objectId)
+                .then(function itemSaved(_deletedObject) {
+                    alert("Item Deleted " + _deletedObject.objectId);
+
+                    return populateList();
+
+                    populateList().then(function (_data) {
+                        $scope.itemsList = _data;
+                    });
+
+
+
+
+                }, function errorDeleting(_error) {
+                    alert("Error Deleting Object " + _objectId)
+                });
+        };
+
+
+
+        $scope.addItem = function addItem() {
+
+            // separate the values
+            var data = $scope.inputItem.value.split(",");
+
+            if (data.length === 2) {
+                $scope.inputItem.name = data[0].trim();
+                $scope.inputItem.room = data[1].trim();
+
+                console.log(JSON.stringify($scope.inputItem));
+
+                ParseHttpService.addObject($scope.inputItem)
+                    .then(function itemSaved(_newItem) {
+                        alert("Item Saved", _newItem.objectId);
+                        $scope.inputItem = {};
+
+                        return populateList();
+
+
+
+                    }, function errorSaving(_error) {
+                        $scope.inputItem = {};
+                    });
+            } else {
+                alert("Invalid Input: " + $scope.inputItem.value);
+                $scope.inputItem = {};
+            }
+        };
+
+        $scope.editObject = function editObject(_object) {
+
+            var data = null;
+            var editedObject = {};
+            var objectData = prompt("Enter the Edited Information", _object.name + ", " + _object.room);
+
+
+            if (objectData != null) {
+                // separate the values
+                data = objectData.split(",");
+            }
+
+            if (objectData && (data.length === 2)) {
+
+                // create object parameters to save
+                editedObject.name = data[0].trim();
+                editedObject.room = data[1].trim();
+                editedObject.objectId = _object.objectId;
+
+                console.log(JSON.stringify(editedObject));
+
+                ParseHttpService.updateObject(editedObject)
+                    .then(function itemUpdated(_updatedItem) {
+                        alert("Item Updated " + _updatedItem.objectId);
+
+                        return populateList();
+
+                    }, function errorSaving(_error) {
+                        alert("Error Editing Object " + _error)
+                    });
+            } else {
+                if (objectData !== null) {
+                    alert("Invalid Input: " + objectData);
+                }
+            }
+        };
 
         function populateList() {
-            return ParseHttpService.getStuff()
+            return ParseHttpService.getStuff().then(function (_listData) {
+                $scope.itemsList = _listData.results;
+            });
         }
 
         console.log('Start')
@@ -37,13 +156,13 @@
         ParseHttpService.login().then(function loginSuccess(_loggedIn) {
             alert(_loggedIn.username + " logged in");
 
-                $scope.handleButtonClick = function() {
-                    populateList().then(function (_data) {
-                        $scope.stuffList = _data;
-                    });
-                }
-        },
-            function error(_error) {
+
+            return ParseHttpService.getStuff();
+        }).then(function getObjectSuccess(_ObjectData){
+                $scope.itemsList = _ObjectData.results;
+
+
+        }, function error(_error) {
                 alert("Error" + _error);
         });
 
@@ -53,6 +172,22 @@
 
 
 
+    });
+
+    a.controller('detailCtrl', function($scope, $state, ParseHttpService){
+        $scope.stateInfo = $state.current;
+
+        $scope.stateParamsInfo = $state.params
+
+        //$scope.stuffDetail = ParseHttpService.getStuffbyid($state.params._id)
+
+        ParseHttpService.getStuffbyid($state.params.objectId).then(function(_data){
+            console.log(_data);
+            console.log('i got something')
+            $scope.item = _data;
+        }, function (_error) {
+            alert("Error". _error.message);
+        });
     });
 
 })();
